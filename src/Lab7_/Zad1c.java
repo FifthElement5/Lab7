d1e4acbb9945022a4240fd85c4af7fdd91d95ca8
FifthElement <i.m.galazewska@gmail.com>
2025-01-18 16:49:36 +0100

    first

--- src/Lab7_/Zad1c.java
+++ src/Lab7_/Zad1c.java
@@ -8,17 +8,17 @@ public class Zad1c {
     	
     	Kalkulator kalk0 = new Kalkulator();
     	System.out.println("test: " + kalk0.getListaLiczb().get(1));
-        // Tworzenie obiektu z jednym argumentem
+      
         Kalkulator kalk1 = new Kalkulator(10);
-        kalk1.zliczanie('+'); 
+        kalk1.zliczanie('-'); 
         kalk1.wypisywanie();
         
-        // Tworzenie obiektu z czterema argumentami
+        
         Kalkulator kalk2 = new Kalkulator(1, 2, 3, 4);
         kalk2.zliczanie('*'); 
         kalk2.wypisywanie();
         
-        // Tworzenie obiektu z siedmioma argumentami
+      
         Kalkulator kalk3 = new Kalkulator(10, 1, 2, 5, 1, 2, 3);
         kalk3.zliczanie('/'); 
         kalk3.wypisywanie();
